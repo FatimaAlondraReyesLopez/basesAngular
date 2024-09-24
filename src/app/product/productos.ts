e8124dbb9371ae1ecbf320cfe07909da40211e07
Fatima Alondra Reyes Lopez <reyeslopezal21@gmail.com>
2024-09-23 20:34:45 -0600

    23 de septiembre

---
+++ src/app/product/productos.ts
@@ -0,0 +1,10 @@
+export interface IProductos {
+    "ProductoId":number;
+      "Modelo":string;
+      "Descripcion":string;
+      "Year":number;
+      "Precio":number;
+      "Color":string;
+      "Marca":string;
+      "ImagenUrl":string;
+}
